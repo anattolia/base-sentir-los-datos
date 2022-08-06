b867636545e48485279c37e95de1315c2a55a087
Antonia Bustamante <anattolia@gmail.com>
2022-08-06 09:12:15 -0500

    :nail_care: Traducir fecha

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import './scss/estilos.scss';
 import datos from './dias.json';
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+import zonaHoraria from 'dayjs/plugin/timezone';
+import 'dayjs/locale/es-mx';
 
 let cuerpo = document.getElementById('cuerpo');
 let boton = document.getElementById('boton');
 
+// Traducir fecha a español y configurar la zona horaria
+dayjs.extend(utc);
+dayjs.extend(zonaHoraria);
+dayjs.locale('es-mx');
+dayjs.tz.setDefault('America/Bogota');
+
 function pintar() {
-  for (let i = 0; i <= 42; i++) {
+  for (let i = 0; i <= datos.length; i++) {
     console.log(datos[i].asesinatos);
     let dia = document.createElement('div');
     let negro = document.createElement('div');
@@ -16,13 +26,18 @@ function pintar() {
     negro.classList.add('negro');
     fecha.classList.add('fecha');
     nombre.classList.add('nombres');
+
     /*  if (lista[i]) {
       negro.innerText = lista[i].nombre;
     } else {
       negro.innerText = '10 de enero, 2022: Nilson Antonio Velásquez Gil';
     } */
     if (datos[i].asesinatos !== 0) {
-      fecha.innerText = `${datos[i].fecha}`;
+      const fechaJS = dayjs(datos[i].fecha);
+      let fechaFormateada = fechaJS.format('MMMM D, YYYY');
+      //fechaFormateada = fechaFormateada.charAt(0).toUpperCase() + fechaFormateada.slice(1);
+
+      fecha.innerText = `${fechaFormateada}`;
       negro.style.width = `${datos[i].asesinatos * 1.5}em`;
       negro.style.height = negro.style.width;
 
